7705ec830d65a7e4dc0c2a138b850f7aed3694ef
oneryalcin <oneryalcin@gmail.com>
2015-11-05 17:41:21 +0000

    Outliers completed and Clustering finished

--- udacity120-intro-machine-learning/outliers/outlier_cleaner.py
+++ udacity120-intro-machine-learning/outliers/outlier_cleaner.py
@@ -10,19 +10,31 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
+    import numpy as np
     
     cleaned_data = []
-
+    error_holder = []
     ### your code goes here
+    # Calculate the residuals
     for i in range(len(predictions)):
-        error = net_worths[i] - predictions[i]
-        print 'Error:',error[0]
-        tpl = (ages[i],net_worths[i],error)
-        cleaned_data.append(tpl)
+        error = (net_worths[i] - predictions[i])**2
+        error_holder.append(error[0])
+        
+    # Sort the errors and select only lower %90 of data 
+    error_holder.sort()
+    #print error_holder
+    #print "Removed errors:", error_holder[len(error_holder)*9/10:]
+    error_holder = error_holder[0:len(error_holder)*9/10]
+     
+    for i in range(len(predictions)):
+        error = (net_worths[i] - predictions[i])**2
+        if error in error_holder:
+            tpl = (ages[i],net_worths[i],error[0])
+            cleaned_data.append(tpl)
 
-    print "Cleaned_data has", len(cleaned_data), "elements"
-    print "Cleaned data error of 5th element:", cleaned_data[5][2][0]
+    #print "Cleaned_data has", len(cleaned_data), "elements"
+    #print "Cleaned data error of 5th element:", cleaned_data[5][2]
+
     
-    ## TODO: write a code to clean %10 of the outlies here
     return cleaned_data
 
